5e0ec5e6e9a8840c628f90d27be2ee6f33e3a4eb
Noah Rahm <correctsyntax@yahoo.com>
2024-12-23 21:58:38 -0600

    Add website and feedback buttons to settings page

--- lib/ui/views/settings/settings_viewmodel.dart
+++ lib/ui/views/settings/settings_viewmodel.dart
@@ -1,8 +1,10 @@
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../app/app.locator.dart';
 import '../../../app/app.router.dart';
+import '../../../common/toast.dart';
 import '../../../services/app_info_service.dart';
 import '../../../services/settings_service.dart';
 
@@ -31,6 +33,25 @@ class SettingsViewModel extends FutureViewModel<String> {
   void changeShowChaptersAndVerses(bool value) {
     _settingsService.setShowChaptersAndVerses(value);
     rebuildUi();
+  }
+
+  void shareFeedback() async {
+    try {
+      await launchUrl(
+        Uri(
+          scheme: 'mailto',
+          path: 'hi@noahrahm.com',
+          queryParameters: {'subject': 'Bibleside app (alpha) feedback'},
+        ),
+      );
+    } catch (e) {
+      // Likely no email client is setup.
+      showToastMsg('Please email to hi@noahrahm.com to share your feedback. Thank you.');
+    }
+  }
+
+  void visitWebsite() async {
+    await launchUrl(Uri.https('bibleside.com'));
   }
 
   Future<String> getAppVersion() async {
