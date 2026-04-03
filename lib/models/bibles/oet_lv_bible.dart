5fb945d82504624dca739933f660757915e3674d
davemhines <dave_hines@taylor.edu>
2026-04-02 20:04:19 -0600

    Updated models/bibles for OET_RV and OET_LV to parse the latest USFM, USJ JSON format. Files were generated by AI and may be buggy.

--- lib/models/bibles/oet_lv_bible.dart
+++ lib/models/bibles/oet_lv_bible.dart
@@ -1,5 +1,6 @@
 import '../../common/enums.dart';
 import '../json_to_bible.dart';
+import '../search_result.dart';
 
 /// The OET Literal Version (OET-LV) implementation.
 ///
@@ -7,7 +8,6 @@ import '../json_to_bible.dart';
 class OETLiteralBibleImpl extends JsonToBible {
   OETLiteralBibleImpl(Map<String, dynamic> json) : super(json: json);
 
-  // TODO: currently replaces all ' marks with ’.
   @override
   Future<String> getBook(
     Area readerArea,
@@ -18,72 +18,92 @@ class OETLiteralBibleImpl extends JsonToBible {
   ) async {
     String htmlText = '';
     String chapterNumberHtml = '';
+    String chapterNumber = '1';
 
-    String chapterNumber = '';
-    List<dynamic> chapterContents = [];
+    String currentVerseId = '';
+    String currentVerseNumber = '';
+    String currentVerseText = '';
+    bool hasPendingVerse = false;
 
-    List<dynamic> chaptersData = json['chapters'];
+    void flushVerse() {
+      if (hasPendingVerse) {
+        String formattedText = currentVerseText
+            .replaceAll(RegExp(r'untr.*?untr\* ?'), '')
+            .replaceAll(RegExp(r'¦([0-9])*\d+'), '')
+            .replaceAll(RegExp(r' +'), ' ')
+            .replaceAll('>', '')
+            .replaceAll('=', '')
+            .replaceAll("'", "’");
 
-    for (Map<String, dynamic> chapter in chaptersData) {
-      chapterNumber = chapter['chapterNumber'];
-
-      chapterNumberHtml = '''<span class="c" id="${readerArea.name}-$bookCode-$chapterNumber">
-          ${showChaptersAndVerses ? chapterNumber : ''}
-          </span>''';
-
-      chapterContents = chapter['contents'];
-
-      // In the current json, the section heading is placed in the verse
-      // before the actual section, so we delay splitting the section with isNext
-      // It means that this is the next verse after the 's1'.
-      bool isNext = false;
-      bool isSection = false;
-      for (Map<String, dynamic> item in chapterContents) {
-        if (isSection == true && isNext == false) {
-          isNext = true;
+        if (!showSpecialMarks) {
+          formattedText = formattedText.replaceAll('_', ' ');
         }
 
-        for (String key in item.keys) {
-          if (key == 's1') {
-            isSection = true;
+        if (currentVerseNumber == '1') {
+          String chapterId = '${readerArea.name}-$bookCode-$chapterNumber';
+          chapterNumberHtml =
+              '\n                <span class="c" id="$chapterId">\n                    ${showChaptersAndVerses ? '$chapterNumber' : ''}\n                </span>';
+        } else {
+          chapterNumberHtml = '';
+        }
+
+        String bookmarkIcon = bookmarkIconHTML(currentVerseId, bookmarks);
+
+        htmlText +=
+            '\n            <p ondblclick="onCreateBookmark(&quot;$currentVerseId&quot;)" class="p">'
+            '$chapterNumberHtml$bookmarkIcon<sup id="$currentVerseId"> ${showChaptersAndVerses ? currentVerseNumber : ''}</sup>&nbsp;${formattedText.trim()}'
+            '\n            </p>';
+
+        hasPendingVerse = false;
+        currentVerseText = '';
+      }
+    }
+
+    if (json['content'] == null) {
+      return htmlText;
+    }
+
+    for (Map item in json['content']) {
+      if (item['type'] == 'chapter') {
+        chapterNumber = item['number'];
+      }
+
+      if (item['type'] == 'para') {
+        if (item['marker'] == 'p' ||
+            item['marker'] == 'm' ||
+            item['marker'] == 'q' ||
+            item['marker'] == 'nb') {
+          for (dynamic contentItem in item['content'] ?? []) {
+            if (contentItem is Map) {
+              if (contentItem['type'] == 'verse') {
+                flushVerse();
+                currentVerseNumber = contentItem['number'];
+                currentVerseId =
+                    '${readerArea.name}-$bookCode-$chapterNumber-$currentVerseNumber';
+                hasPendingVerse = true;
+              } else if (contentItem['type'] == 'char') {
+                for (dynamic charItem in contentItem['content'] ?? []) {
+                  if (charItem is String) currentVerseText += charItem;
+                }
+              }
+            } else if (contentItem is String) {
+              if (hasPendingVerse) {
+                currentVerseText += contentItem;
+              }
+            }
           }
-
-          // Handle verse numbers
-          if (key == 'verseNumber') {
-            String verseNumberText = item[key];
-
-            if (verseNumberText != '1') {
-              chapterNumberHtml = '';
-            }
-
-            if (isSection == false) {
-              String verseId = '${readerArea.name}-$bookCode-$chapterNumber-$verseNumberText';
-              String bookmarkIcon = bookmarkIconHTML(verseId, bookmarks);
-
-              htmlText += '''<p ondblclick=onCreateBookmark("$verseId") class="p">
-                  $chapterNumberHtml$bookmarkIcon<sup id="$verseId"> ${showChaptersAndVerses ? verseNumberText : ''}</sup>''';
-            }
-          } else if (key == 'verseText') {
-            // Note: we remove numbers and markings related to links for now
-            String verseText;
-
-            verseText = item[key]
-                .replaceAll(RegExp(r'¦([0-9])*\d+'), '')
-                .replaceAll(' +', ' ')
-                .replaceAll('>', ' ')
-                .replaceAll('=', ' ');
-
-            // Remove OET-LV special markings like _.
-            if (showSpecialMarks == false) {
-              verseText = verseText.replaceAll('_', ' ');
-            }
-
-            htmlText += "&nbsp;${verseText.replaceAll("'", "’")}</span>";
-          }
+          flushVerse(); // Ensure we flush the last verse of the paragraph
         }
       }
     }
 
-    return htmlText;
+    return htmlText.trimLeft() + '\n';
+  }
+
+  @override
+  Future<List<SearchResult>> getSearchResults(
+      String bookCode, String searchTerm) async {
+    // Search implementation for verses (handled globally or ignored here if searchHeaders is needed)
+    return super.getSearchResults(bookCode, searchTerm);
   }
 }
